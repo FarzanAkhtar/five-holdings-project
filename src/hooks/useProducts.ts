36690608ddae0dc4dd6c8b13970e554f4ded3b38
FarzanAkhtar <farzanakhtar.it19@gmail.com>
2026-01-06 21:25:02 +0530

    commented custom hook implementation

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -8,7 +8,7 @@ export function useProducts(categorySlug: string | null) {
     queryFn: ({ pageParam = 0 }) =>
       fetchProductsByCategory(categorySlug!, {
         limit: PRODUCTS_PER_PAGE,
-        skip: pageParam,
+        skip: pageParam
       }),
     initialPageParam: 0,
     getNextPageParam: (lastPage) => {
@@ -19,3 +19,56 @@ export function useProducts(categorySlug: string | null) {
     staleTime: 2 * 60 * 1000,
   });
 }
+
+
+// "products_beauty_timestamp"
+// state: key value
+
+// useInfiniteQuery: (key, fn) {
+//   state = memutil()
+//   if (key in state) {
+
+//   } else {
+//     const val = fn()
+//     state[key] = val
+// }
+
+// interface Result {
+//   data: Object,
+//   isLoading: Boolean
+// }
+
+// const useCustomInfiniteQuery = (
+//   keys: Array<String>,
+//   fn: Function
+// ) => {
+//   const state = getState()
+//   const [isLoading, setIsLoading] = useState(true)
+//   const [value, setValue] = useState()
+//   // const primary_key = keys.slice(0, 2).join("_")
+//   const rest_keys = keys.slice(2).join("_")
+//   // if (state[primary_key] != null) {
+    
+//   // } else {
+//     setIsLoading(true)
+//     fn().then((value) => {
+//       let primaryState = state[primary_key]
+//       const newState = { ...state,
+//       primary_key : {
+//         ...primaryState,
+//         rest_keys: value
+//       }
+//       }
+//       setState(newState)
+//       setIsLoading(false)
+//       setValue(value)
+//     }
+//     ).catch(() => {
+//       setIsLoading(false)
+
+//       setIsValue({})
+//     }
+//     )
+//     return { loading, value }
+//   // }
+// }
